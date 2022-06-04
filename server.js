3c7c7b60f2cafdaef4589514a55122617adbaee2
Paphana Yiwsiw <paphana.yiwsiw@gmail.com>
2022-06-05 00:29:11 +0700

    add dotenv and get data from CMS API

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const { json } = require('express/lib/response');
+const { json, header } = require('express/lib/response');
 
 const http = require('http'),
     express = require('express'),
     bodyParser = require('body-parser'),
     axios = require('axios').default,
+    dotenv = require('dotenv'),
     app = express();
 
+dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PATH = __dirname + '/client';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.use(express.static(PATH));
 http.createServer(app).listen(PORT, () => {
     console.log('Server listening on http://localhost:' + PORT);
@@ -25,6 +27,7 @@ app.get('/', (_req, res) => {
     res.status(200).sendFile(PATH + '/index.html');
 });
 
+// Get data from NASA POWER API
 app.get('/get-nasa-data', (req, res) => {
     //if query is empty return error
     if(!req.query.start || !req.query.end) {
@@ -49,6 +52,40 @@ app.get('/get-nasa-data', (req, res) => {
             }
         }).then(response => {
             res.status(200).send(response.data);
+        }).catch(error => {
+            res.status(500).send(error);
+        });
+    }
+});
+
+// Get data from CMS API
+app.get('/get-cms-data', (req, res) => {
+    //if query is empty return error
+    //note that start and end is the UNIX timestamp
+    if(!req.query.start || !req.query.end) {
+        res.status(400).send({message: 'Please provide a start and end date'});
+    }else{
+        const base_url = "https://siit.pdxeng.ch:8000/cms/api/v1.0";
+        const auth = {"username": process.env.CMS_UNAME, 
+                    "password": process.env.CMS_PWD, 
+                    "cms_uid": process.env.CMS_UID};
+        //get token
+        axios.post(base_url + "/token", auth).then(response => {
+            const token = response.data.token,
+                  head = { "Authorization": "Bearer " + token},
+                  url = base_url+"/reports/devices/"+process.env.CMS_DEVICE_UID+"/objects/"+process.env.CMS_OBJECT_NAME;
+            //get data
+            axios.get(url, {
+                headers: head,
+                params: {
+                    from: req.query.start,
+                    to: req.query.end
+                }
+            }).then(response2 => {
+                res.status(200).send(response2.data.values);
+            }).catch(error => {
+                res.status(500).send(error);
+            });
         }).catch(error => {
             res.status(500).send(error);
         });
