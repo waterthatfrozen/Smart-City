2c2852c094c896084ca5ba06a5c3a395c7120240
Paphana Yiwsiw <paphana.yiwsiw@gmail.com>
2022-06-07 00:17:31 +0700

    add firebase

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const { json, header } = require('express/lib/response');
+const { exit } = require('process');
 
 const http = require('http'),
     express = require('express'),
     bodyParser = require('body-parser'),
     axios = require('axios').default,
+    firebaseApp = require('firebase/app'),
+    firebaseAnalytics = require('firebase/analytics'),
+    firebaseFunctions = require('firebase/functions'),
     dotenv = require('dotenv'),
     app = express();
 
@@ -11,8 +14,28 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Firebase Configurations
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MSGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const firebase = firebaseApp.initializeApp(firebaseConfig);
+if (firebase) {
+    console.log('Firebase app initialized');
+}else{
+    console.log('Firebase app not initialized');
+    exit(1);
+}
+
 const PATH = __dirname + '/client';
 const PORT = process.env.PORT;
+
 app.use(express.static(PATH));
 http.createServer(app).listen(PORT, () => {
     console.log('Server listening on http://localhost:' + PORT);
@@ -33,7 +56,7 @@ app.get('/get-nasa-data', (req, res) => {
     if(!req.query.start || !req.query.end) {
         res.status(400).send({message: 'Please provide a start and end date'});
         }else{
-        var START = req.query.start, END = req.query.end;
+        const START = req.query.start, END = req.query.end;
         const FORMAT = "JSON",
             LATITUDE = 14.067453,
             LONGITUDE = 100.605089,
@@ -59,7 +82,7 @@ app.get('/get-nasa-data', (req, res) => {
 });
 
 // Get data from CMS API
-app.get('/get-cms-data', (req, res) => {
+app.get('/get-env-sensor-data', (req, res) => {
     //if query is empty return error
     //note that start and end is the UNIX timestamp
     if(!req.query.start || !req.query.end) {
@@ -73,7 +96,7 @@ app.get('/get-cms-data', (req, res) => {
         axios.post(base_url + "/token", auth).then(response => {
             const token = response.data.token,
                   head = { "Authorization": "Bearer " + token},
-                  url = base_url+"/reports/devices/"+process.env.CMS_DEVICE_UID+"/objects/"+process.env.CMS_OBJECT_NAME;
+                  url = base_url+"/reports/devices/"+process.env.CMS_ENVSNR_DEVICE_UID+"/objects/"+process.env.CMS_ENVSNR_OBJECT_NAME;
             //get data
             axios.get(url, {
                 headers: head,
