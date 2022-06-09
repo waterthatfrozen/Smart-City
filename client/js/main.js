32f12ff1d96776389da3c1b6ca021bfb462b321e
Paphana Yiwsiw <paphana.yiwsiw@gmail.com>
2022-06-09 17:06:47 +0700

    leading zeros in hours and minutes

--- client/js/main.js
+++ client/js/main.js
@@ -3,8 +3,10 @@ function datetimeUpdate() {
     var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var date = new Date();
     var dateString = weekday[date.getDay()] + ", " + date.getDate() + " " + month[date.getMonth()] + " " + date.getFullYear();
+    var hourString = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    var minuteString = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
     var secondsString = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
-    var timeString = date.getHours() + ":" + date.getMinutes() + ":" + secondsString;
+    var timeString = hourString + ":" + minuteString + ":" + secondsString;
     var datetimeString = dateString + " " + timeString;
     $("#datetime-display").html(datetimeString);
 }
